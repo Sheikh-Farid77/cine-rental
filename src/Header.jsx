5daab97aeeccadaf01d8d18809d02e92b35c7b44
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-03-09 10:10:20 +0600

    cart features implemented by context api

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,24 @@ import logo from "../src/assets/logo.svg";
 import ring from "../src/assets/ring.svg";
 import moon from "../src/assets/icons/moon.svg";
 import shoppingCart from "../src/assets/shopping-cart.svg";
+import { useContext, useState } from "react";
+import CartDetails from "./cine/CartDetails";
+import { MovieContext } from "./context";
 
 export default function Header() {
+  const [showCart, setShowCArt] = useState(false);
+  const { cartData } = useContext(MovieContext);
+
+  const handleShowCart = () => {
+    setShowCArt(true);
+  };
+
+  const handleCloseCart = () => {
+    setShowCArt(false);
+  };
   return (
     <header>
+      {showCart && <CartDetails onCloseCart={handleCloseCart} />}
       <nav className="container mx-auto flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={logo} width="139" height="26" alt="" />
@@ -30,10 +44,12 @@ export default function Header() {
           </li>
           <li>
             <a
+              onClick={handleShowCart}
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
               <img src={shoppingCart} width="24" height="24" alt="" />
+              {cartData.length > 0 && <span className="flex justify-center items-center rounded-full absolute top-[-10px] md:top-[-12px] left-[20px] md:left-[28px] bg-[#12CF6F] text-white text-center text-xs md:text-base w-[20px] h-[20px] md:w-[30px] md:h-[30px]">{cartData.length}</span>}
             </a>
           </li>
         </ul>
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { getImgUrl } from "../utils/cine-utility";
 import Rating from "./Rating";
 import MovieDetailsModal from "./MovieDetailsModal";
+import { MovieContext } from "../context";
 
 export default function MovieCard({ movie }) {
   const [showModal, setShowModal] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const { cartData, setCartData } = useContext(MovieContext);
 
   const handleModalClose = () => {
     setSelectedMovie(null);
@@ -14,7 +16,19 @@ export default function MovieCard({ movie }) {
 
   const handleMovieSelection = (movie) => {
     setSelectedMovie(movie);
-    setShowModal(true)
+    setShowModal(true);
+  };
+
+  const handleAddToCart = (event, movie) => {
+    event.stopPropagation();
+
+    const found = cartData.find((d) => {
+      return d.id === movie.id;
+    });
+
+    if (!found) {
+      setCartData([...cartData, movie]);
+    }
   };
   return (
     <>
@@ -22,6 +36,7 @@ export default function MovieCard({ movie }) {
         <MovieDetailsModal
           movie={selectedMovie}
           onModalClose={handleModalClose}
+          onAddToCart = {handleAddToCart}
         />
       ) : (
         <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
@@ -37,13 +52,14 @@ export default function MovieCard({ movie }) {
               <div className="flex items-center space-x-1 mb-5">
                 <Rating value={movie.rating} />
               </div>
-              <a
+              <button
                 className="primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                 href="#"
+                onClick={(e) => handleAddToCart(e, movie)}
               >
                 <img src="./assets/tag.svg" alt="" />
                 <span>${movie.price} | Add to Cart</span>
-              </a>
+              </button>
             </figcaption>
           </a>
         </figure>
